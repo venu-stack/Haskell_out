eb0d6edb9ee0ec79914a1c97bd4589c324ef6520
venu Borra <venuborra@icloud.com>
2021-10-30 20:11:32 +0530

    Custome Data

--- dts.hs
+++ dts.hs
@@ -1,3 +1,9 @@
 data Shape = Circle Float Float Float | Rectangle Float Float Float Float deriving (Show)
 surface :: Shape -> Float 
 surface (Circle _ _ r)= pi *r 
+
+data Person = Person {firstName :: String, 
+                      lastName :: String,
+                      age :: Int,
+                      phoneNumber :: String,
+                      flavor::String}deriving (Show)
